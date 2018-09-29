b77f2b90224a573b5e8d01c86489663a6a96cf09
Florian Kinder <florian.kinder@fankserver.com>
2018-09-29 22:19:28 +0200

    Fixed threading issues

--- unique.go
+++ unique.go
@@ -1,12 +1,18 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
 type UniqueRand struct {
+	lock      sync.Mutex
 	generated map[uint64]bool
 }
 
 func (u *UniqueRand) UInt64() uint64 {
+	u.lock.Lock()
+	defer u.lock.Unlock()
 	for {
 		i := rand.Uint64()
 		if !u.generated[i] {
@@ -17,5 +23,7 @@ func (u *UniqueRand) UInt64() uint64 {
 }
 
 func (u *UniqueRand) Reset() {
+	u.lock.Lock()
+	defer u.lock.Unlock()
 	u.generated = map[uint64]bool{}
 }
